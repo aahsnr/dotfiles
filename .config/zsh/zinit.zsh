d507d964a36783ab2648c8aaaa5f359c8edeff85
aahsnr <ahsanur041@proton.me>
2024-12-16 10:54:27 +0600

    removed submodules

--- .config/zsh/zinit.zsh
+++ .config/zsh/zinit.zsh
@@ -1,18 +1,15 @@
 #!/bin/zsh
 
 # Add in zsh plugins
-zinit light zsh-users/zsh-syntax-highlighting
-zinit light zsh-users/zsh-completions
-zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
-zinit light hlissner/zsh-autopair
+
+
+source /usr/share/zsh/plugins/zsh-autopair/autopair.zsh
 
 # Add in snippets
 zinit snippet OMZP::git
 zinit snippet OMZP::sudo
 zinit snippet OMZP::archlinux
-zinit snippet OMZP::kubectl
-zinit snippet OMZP::kubectx
 zinit snippet OMZP::command-not-found
 
 zinit cdreplay -q
